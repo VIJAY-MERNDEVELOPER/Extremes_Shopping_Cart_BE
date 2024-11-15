f4bd65a83cfd6ca4eafc8d693bc9e545256c7011
VIJAY-MERNDEVELOPER <142765666+VIJAY-MERNDEVELOPER@users.noreply.github.com>
2024-11-15 22:02:12 +0530

    cookies setup

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const secretkey = process.env.SECRET_KEY;
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://extremes-shopping-cart-fe.onrender.com",
   })
 );
 
